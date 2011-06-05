eeb0e31d0bfe43fb9d2317d2c1d559d5c5649d53
Stephan Mann <stephan@atomictagging.org>
2011-06-05 15:49:58 +0200

    cleanup

--- org.atomictagging.core/src/org/atomictagging/core/internal/Activator.java
+++ org.atomictagging.core/src/org/atomictagging/core/internal/Activator.java
@@ -1,29 +1,40 @@
+/**
+ * This file is part of Atomic Tagging.
+ * 
+ * Atomic Tagging is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Atomic Tagging is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with Atomic Tagging. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.atomictagging.core.internal;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+/** */
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	private static BundleContext	context;
+
 
 	static BundleContext getContext() {
 		return context;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
+
+	@Override
+	public void start( BundleContext bundleContext ) throws Exception {
 		Activator.context = bundleContext;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
+
+	@Override
+	public void stop( BundleContext bundleContext ) throws Exception {
 		Activator.context = null;
 	}
 
